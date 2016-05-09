a5fc0487b5f0e7f2048d2979a2b59868567430b6
Carlos Villa <uo236851@uniovi.es>
2016-05-09 19:36:00 +0200

    Commit de 4 Gb

---
+++ sdi3-13.Cli-REST/src/com/sdi/client/Main.java
@@ -0,0 +1,16 @@
+package com.sdi.client;
+
+public class Main {
+	
+	private static final String REST_TRIP_SERVICE_URL = "http://localhost:8280"
+			+ "/sdi3-13.Web/rest/TripsServiceRs";
+
+	public static void main(String[] args) {
+		new Main().run();
+	}
+
+	private void run() {
+
+	}
+
+}
