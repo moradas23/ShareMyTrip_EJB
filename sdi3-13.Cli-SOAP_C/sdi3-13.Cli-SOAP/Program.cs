1622a8839f99ce8c3e0dd026445b5dda63aa4619
Carlos Villa <uo236851@uniovi.es>
2016-05-14 13:20:26 +0200

    Cliente WS C#

---
+++ sdi3-13.Cli-SOAP_C/sdi3-13.Cli-SOAP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdi3_13.Cli_SOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EjbUserServiceService userService = new EjbUserServiceService();
+
+            user[] usuarios = userService.getUsers();
+
+
+            foreach (user usuario in usuarios)
+            {
+                Console.WriteLine(usuario.name);
+            }
+
+            Console.Write("Pulse un botón para terminar");
+            Console.Read();
+            
+
+
+        }
+    }
+}
